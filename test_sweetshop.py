6cb562ff1c4dc40e47d96f783113c9df647a823d
Devansh1135 <devanshjagatiya1135@gmail.com>
2025-07-17 13:22:06 +0530

    Added failing case for delete_sweet

--- test_sweetshop.py
+++ test_sweetshop.py
@@ -9,6 +9,16 @@ class TestSweetShop(unittest.TestCase):
         self.assertEqual(len(sweets),1)
         self.assertEqual(sweets[0].sweet_name,"Kaju katli")
         self.assertEqual(sweets[0].sweet_price_per_kg,900)
+
+    def test_delete_sweet(self):
+        shop = SweetShop()
+        shop.add_sweet(1,"Kaju Katli","sweet",900,5)
+        shop.add_sweet(2,"Gulab jamun","sweet",500,10)
+        shop.delete_sweet(1)
+        sweets = shop.get_all_sweets()
+        self.assertEqual(len(sweets),1)
+        self.assertEqual(sweets[0].sweet_id,2)
+
         
 if __name__ == '__main__':
     unittest.main()
