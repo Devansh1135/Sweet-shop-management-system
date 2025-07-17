bc4185b768edb095a52e8dc00947774c4a868746
Devansh1135 <devanshjagatiya1135@gmail.com>
2025-07-17 13:37:19 +0530

    Implmented delete_sweet and make delete_sweet test pass

--- sweet_shop.py
+++ sweet_shop.py
@@ -8,5 +8,12 @@ class SweetShop:
         sweet = Sweet(id,name,category,price,quantity)
         self.sweets.append(sweet)
 
+    def delete_sweet(self, id):
+        for sweet in self.sweets:
+            if sweet.sweet_id == id:
+                self.sweets.remove(sweet)
+                return True
+        return False
+
     def get_all_sweets(self):
         return self.sweets
