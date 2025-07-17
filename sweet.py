7508b58053ab96fc81ad1670b95adcda99c9c66f
Devansh1135 <devanshjagatiya1135@gmail.com>
2025-07-17 13:11:48 +0530

    Implement add_sweet, get_all_sweets and make their tests pass

--- sweet.py
+++ sweet.py
@@ -1,3 +1,8 @@
 class Sweet:
-    def __init__(self):
-        pass
+    def __init__(self, id : int, name : str ,category : str, price : int, quantity : int):
+        self.sweet_id = id
+        self.sweet_name = name
+        self.sweet_category = category
+        self.sweet_price_per_kg = price
+        self.sweet_in_stock = quantity
+        
--- sweet_shop.py
+++ sweet_shop.py
@@ -1,3 +1,12 @@
+from sweet import Sweet
+
 class SweetShop:
     def __init__(self):
         self.sweets = []
+
+    def add_sweet(self, id,name,category,price,quantity):
+        sweet = Sweet(id,name,category,price,quantity)
+        self.sweets.append(sweet)
+
+    def get_all_sweets(self):
+        return self.sweets
